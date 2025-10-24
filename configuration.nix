5eb7fd306fb6aa84db01119ea9fd1fde46068f5f
lucasterly <67102059+lucasterly@users.noreply.github.com>
2025-10-24 22:41:59 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -6,18 +6,18 @@
     [ 
     	./modules/hardware-configuration.nix
 	./services/gitea.nix
-	./services/samba.nix
-#	./modules/nvidia.nix
+#	./services/samba.nix
+	./modules/nvidia.nix
 #	./modules/virt.nix
 #	./services/sunshine.nix
 	./services/tail.nix
-#	./modules/desktop.nix
+	./modules/desktop.nix
 	./modules/packages.nix
 #	./services/vault.nix
 #	./services/nextclound.nix
 #	./modules/disk.nix
 #	./services/jelly.nix
-	./modules/network.nix
+#	./modules/network.nix
 	./modules/linux.nix
 	./modules/lang.nix
 	./modules/users.nix
